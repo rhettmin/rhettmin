fe0a79ab1239a6f975daad2feec44c6294a4e8cf
rhetmin <110890562+rhettmin@users.noreply.github.com>
2022-08-11 15:47:38 +0900

    Add files via upload

---
+++ 0811/js/myjs.js
@@ -0,0 +1,8 @@
+//variable
+/*variable*/
+var age=prompt("How old are you?","20");
+console.log(age);
+//if the terms are right
+if (age >= 20) {document.write ("<h3>You are an adult</h3>");} 
+//if the teras are false
+          else {document.write ("<h3>You are not an Adult</h3>");}   
